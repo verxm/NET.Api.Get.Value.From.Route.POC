a2f81eead1abcb6e08edd0e3ae713333933ef48e
Davi Santos <davisantos448@gmail.com>
2025-05-29 16:02:12 -0300

    chore(TicketController): remove unused properties and constructor.

--- src/Api/Controllers/TicketController.cs
+++ src/Api/Controllers/TicketController.cs
@@ -6,24 +6,12 @@ namespace NET.Api.Get.Value.From.Route.POC.Controllers
     [Route("v1")]
     public class TicketController : ControllerBase
     {
-        private static readonly string[] Summaries = new[]
-        {
-            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-        };
-
-        private readonly ILogger<TicketController> _logger;
-
-        public TicketController(ILogger<TicketController> logger)
-        {
-            _logger = logger;
-        }
-
         [HttpGet("tickets/{ticketId}")]
         public IActionResult Get([FromRoute] string ticketId)
         {
             var ticketIdFromRoute = HttpContext.GetRouteValue("ticketid");
 
-            return Ok(new {ticketId, ticketIdFromRoute});
+            return Ok(new { ticketId, ticketIdFromRoute });
         }
     }
 }
